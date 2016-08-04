aac27f0799b499c3d68275c679319dd0d2d2bece
vzuevskiy <vzuevskiy@gmail.com>
2016-08-04 23:04:05 +0700

    Добавлена индикация состояния устройства и немного мелких правок

--- GateRF13.ino
+++ GateRF13.ino
@@ -3,18 +3,30 @@
 #include <MFRC522.h>
 #include <EEPROM2.h>
 #include <EthernetUdp.h>
+/*
+ * pin 2    - SS(SDA)
+ * pin 3    - RST
+ * pin 5    - relay
+ * pin 7    - button
+ * pin 8    - led 1
+ * pin 9    - led 2
+ * pin 11   - mosi
+ * pin 12   - miso
+ * pin 13   - sck
+ * 
+*/
 
+#define ledPin_0 8   /*Пин 0 Диодика*/
+#define ledPin_1 9   /*Пин 1 Диодика*/
 #define RST_PIN 2
-#define SS_PIN 3
+#define SS_PIN 3    
 #define RelayPin 5  /*Пин замка*/
-#define BeepPin 4   /*Пин Пищалки*/
-#define ledPin 6    /*Пин Диодика*/
-#define btnPin 7    /*Пин кнопки выхода*/
+#define btnPin 6    /*Пин кнопки выхода*/
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 EthernetUDP udp;
 EthernetClient client;
-IPAddress server(192, 168, 0, 155);
+IPAddress server(192, 168, 0, 37);
 unsigned long uidDec, uidDecTemp;  // для храниения номера метки в десятичном формате
 unsigned long lastAttemptTime = 0;
 const unsigned long updateInterval = 60000; // интервал обновления мастер клюей мс
@@ -22,23 +34,27 @@ bool btnState = 0;                  // Состояние кнопки, 1 ког
 bool MasterUID = 0;
 String incomingString;
 
-
 void initializePins() {
+  pinMode(ledPin_0, OUTPUT);
+  pinMode(ledPin_1, OUTPUT);
   pinMode(RelayPin, OUTPUT);
-  pinMode(BeepPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
   pinMode(btnPin, INPUT);
   pinMode(SS_PIN, OUTPUT); // рфид
   pinMode(10, OUTPUT);     // езернет
-  digitalWrite(RelayPin, 1);
+  digitalWrite(RelayPin, HIGH);
+  digitalWrite(ledPin_0,  HIGH);
 }
 
 void initializeEthernet() {
+  /*
+   * горит ledPin_0 - ip не получен, переход на ledPin_1 - ip получен
+  */
   byte mac[] = { 0xDE, 0xAD, 0xBE, 0xFF, 0xEF, 0xED };
   digitalWrite(SS_PIN, HIGH); // откл рфид
   while (!Ethernet.begin(mac));
   Serial.println(Ethernet.localIP());
-
+  digitalWrite(ledPin_0, LOW);
+  digitalWrite(ledPin_1, HIGH);
 }
 
 void openDoor(byte inside) {
@@ -49,6 +65,8 @@ void openDoor(byte inside) {
   unsigned long opentime = 0;
   if (inside == 0) {
     if (client.connect(server, 7364)) {
+      digitalWrite(ledPin_0, LOW);
+      digitalWrite(ledPin_1, HIGH);
       client.print("GATE,");
       client.print("INSIDE,");
       client.println(uidDec);
@@ -79,6 +97,8 @@ void openDoor(byte inside) {
       client.stop();
     } else {
       Serial.println("connection failed");
+      digitalWrite(ledPin_0, HIGH);
+      digitalWrite(ledPin_1, HIGH);
       if (MasterUID) {
         digitalWrite(RelayPin, 0); // размыкаем релешку
         opentime = millis();
@@ -111,6 +131,8 @@ void updUIDs() {
   unsigned long countUIDs, fromEEPROM;
 
     if (client.connect(server, 7364)) {
+      digitalWrite(ledPin_0, LOW);
+      digitalWrite(ledPin_1, HIGH);
       client.print("GATE,");
       client.println("UPD");
       int i = 0;
@@ -153,18 +175,32 @@ void updUIDs() {
       Serial.println("stop");
     } else {
       Serial.println("UPD err: connection failed.");
+      digitalWrite(ledPin_0, HIGH);
+      digitalWrite(ledPin_1, HIGH);
     }
     lastAttemptTime = millis();
   }
 
 void sendNewUID(unsigned long UID) {
+  /*
+   * При успешной отправке ключа зажигаемм led0=1 && led1=0, 350mc, led0=0 && led1=1
+  */
   if (client.connect(server, 7364)) {
+    digitalWrite(ledPin_0, LOW);
+    digitalWrite(ledPin_1, HIGH);
     client.print("GATE,");
     client.print("NEW,");
     client.println(uidDec);
     client.stop();
+    digitalWrite(ledPin_0, HIGH);
+    digitalWrite(ledPin_1, LOW);
+    delay(350);
+    digitalWrite(ledPin_0, LOW);
+    digitalWrite(ledPin_1, HIGH);
   } else {
     Serial.println("connection failed");
+    digitalWrite(ledPin_0, HIGH);
+    digitalWrite(ledPin_1, HIGH);
   }
 }
 
